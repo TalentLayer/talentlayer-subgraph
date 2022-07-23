6cdc841eda09d99003a2b6bdaf96bf1f7ff09028
Michael Mueller <hello@michaelmueller.dev>
2022-07-23 16:34:34 +0200

    add TalentLayerId contract and User entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,80 +1,26 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from '@graphprotocol/graph-ts';
 import {
-  TalentLayerID,
   Approval,
   ApprovalForAll,
   ConsecutiveTransfer,
   Mint,
   OwnershipTransferred,
-  Transfer
-} from "../generated/TalentLayerID/TalentLayerID"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+} from '../generated/TalentLayerID/TalentLayerID';
+import { createAndGetUser } from './getters';
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.handles(...)
-  // - contract.hasBeenRecovered(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.isTokenPohRegistered(...)
-  // - contract.name(...)
-  // - contract.numberMinted(...)
-  // - contract.owner(...)
-  // - contract.ownerOf(...)
-  // - contract.pohRegistry(...)
-  // - contract.profilesDataUri(...)
-  // - contract.recoveryRoot(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.talentIdPohAddresses(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-  // - contract.walletOfOwner(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleConsecutiveTransfer(event: ConsecutiveTransfer): void {}
 
-export function handleMint(event: Mint): void {}
+export function handleMint(event: Mint): void {
+  let user = createAndGetUser(event.params._user);
+  user.userTokenId = event.params._tokenId;
+  user.handle = event.params._handle;
+  user.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
