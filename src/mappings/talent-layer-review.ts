d9d46f7f42c809b5edf32d59b09a536b8428460d
Michael Mueller <hello@michaelmueller.dev>
2022-07-23 20:12:11 +0200

    convert Review toId to User, add reviews reverse lookup

--- src/mappings/talent-layer-review.ts
+++ src/mappings/talent-layer-review.ts
@@ -12,9 +12,9 @@ export function handleApproval(event: Approval): void {}
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleMint(event: Mint): void {
-  let review = createAndGetReview(event.params._tokenId);
+  let review = createAndGetReview(event.params._tokenId, event.params._toId);
   review.jobId = event.params._jobId;
-  review.toId = event.params._toId;
+  // review.toId = event.params._toId.toString();
   review.uri = event.params._reviewUri;
   review.save();
 }
