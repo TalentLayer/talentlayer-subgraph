6821e4d4e84b8a3b19fc649f471c8b6e7ce67201
Akumaw <dev@akumaw.com>
2022-11-09 13:42:55 +0100

    Clean prettier

--- src/mappings/talentLayerMultipleArbitrableTransaction.ts
+++ src/mappings/talentLayerMultipleArbitrableTransaction.ts
@@ -1,56 +1,52 @@
-import { log } from "@graphprotocol/graph-ts";
-import {Service, User} from "../../generated/schema";
+import { log } from '@graphprotocol/graph-ts'
+import { Service, User } from '../../generated/schema'
+import { getOrCreateService, getOrCreatePayment, getOrCreateProposal } from '../getters'
 import {
-  getOrCreateService,
-  getOrCreatePayment,
-  getOrCreateProposal
-} from "../getters";
-import {
-  ServiceProposalConfirmedWithDeposit, Payment,
+  ServiceProposalConfirmedWithDeposit,
+  Payment,
   PaymentCompleted,
-} from "../../generated/TalentLayerMultipleArbitrableTransaction/TalentLayerMultipleArbitrableTransaction";
-import {generateProposalId, generateUniqueId} from "./utils";
+} from '../../generated/TalentLayerMultipleArbitrableTransaction/TalentLayerMultipleArbitrableTransaction'
+import { generateProposalId, generateUniqueId } from './utils'
 
-export function handleServiceProposalConfirmedWithDeposit(
-    event: ServiceProposalConfirmedWithDeposit): void {
-  const service = getOrCreateService(event.params.serviceId);
+export function handleServiceProposalConfirmedWithDeposit(event: ServiceProposalConfirmedWithDeposit): void {
+  const service = getOrCreateService(event.params.serviceId)
 
-  const proposalId = generateProposalId(event.params.serviceId.toString(), event.params.sellerId.toString());
-  const proposal = getOrCreateProposal(proposalId);
+  const proposalId = generateProposalId(event.params.serviceId.toString(), event.params.sellerId.toString())
+  const proposal = getOrCreateProposal(proposalId)
 
-  log.warning("!!!!!! proposal ID", [proposalId]);
-  log.warning("!!!!!! service ID", [event.params.serviceId.toString()]);
+  log.warning('!!!!!! proposal ID', [proposalId])
+  log.warning('!!!!!! service ID', [event.params.serviceId.toString()])
 
-  service.status = "Confirmed";
-  service.transactionId = event.params.transactionId.toString();
-  service.seller = User.load(event.params.sellerId.toString())!.id;
-  service.save();
+  service.status = 'Confirmed'
+  service.transactionId = event.params.transactionId.toString()
+  service.seller = User.load(event.params.sellerId.toString())!.id
+  service.save()
 
-  proposal.status = "Validated";
-  proposal.save();
+  proposal.status = 'Validated'
+  proposal.save()
 }
 
 export function handlePaymentCompleted(event: PaymentCompleted): void {
-  const service = getOrCreateService(event.params._serviceId);
-  service.status = "Finished";
-  service.save();
+  const service = getOrCreateService(event.params._serviceId)
+  service.status = 'Finished'
+  service.save()
 }
 
 export function handlePayment(event: Payment): void {
-  const paymentId = generateUniqueId(event.transaction.hash.toHex(), event.logIndex.toString());
-  const payment = getOrCreatePayment(paymentId, event.params._serviceId);
+  const paymentId = generateUniqueId(event.transaction.hash.toHex(), event.logIndex.toString())
+  const payment = getOrCreatePayment(paymentId, event.params._serviceId)
 
-  payment.service = Service.load(event.params._serviceId.toString())!.id;
-  payment.amount = event.params._amount;
-  payment.rateToken = event.params._token;
+  payment.service = Service.load(event.params._serviceId.toString())!.id
+  payment.amount = event.params._amount
+  payment.rateToken = event.params._token
 
-  if(event.params._paymentType === 0){
-    payment.paymentType = 'Release';
+  if (event.params._paymentType === 0) {
+    payment.paymentType = 'Release'
   }
-  if(event.params._paymentType === 1){
-    payment.paymentType = 'Reimburse';
+  if (event.params._paymentType === 1) {
+    payment.paymentType = 'Reimburse'
   }
 
-  payment.transactionHash = event.transaction.hash.toHex();
-  payment.save();
+  payment.transactionHash = event.transaction.hash.toHex()
+  payment.save()
 }
