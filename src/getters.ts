0310743287562843efff0e8dd1a46006206c73f9
Dercio <contact@creedscode.cc>
2022-11-22 06:59:56 +0100

    feat: :sparkles: Add user total gain

--- src/getters.ts
+++ src/getters.ts
@@ -10,6 +10,7 @@ import {
   FeeClaim,
   FeePayment,
   PlatformGain,
+  UserGain,
 } from '../generated/schema'
 import { ZERO, ZERO_ADDRESS, ZERO_BIGDEC, ZERO_TOKEN_ADDRESS } from './constants'
 import { ERC20 } from '../generated/TalentLayerMultipleArbitrableTransaction/ERC20'
@@ -176,4 +177,14 @@ export function getOrCreatePlatformGain(gainId: string): PlatformGain {
     platformGain.save()
   }
   return platformGain
+}
+
+export function getOrCreateUserGain(gainId: string): UserGain {
+  let userGain = UserGain.load(gainId)
+  if (!userGain) {
+    userGain = new UserGain(gainId)
+    userGain.totalUserGain = ZERO
+    userGain.save()
+  }
+  return userGain
 }
