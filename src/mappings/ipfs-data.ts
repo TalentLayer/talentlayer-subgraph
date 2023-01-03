aaf0b424e53a70fc5f7360a3cab4ee0dcf6eaa3a
Emil Madrell <empasa@tuta.io>
2023-01-03 09:30:33 +0100

    Added ipfs search for reviews

--- src/mappings/ipfs-data.ts
+++ src/mappings/ipfs-data.ts
@@ -1,5 +1,5 @@
 import { log, ipfs, json, Bytes, dataSource } from '@graphprotocol/graph-ts'
-import { ServiceDescription, ProposalDescription } from '../../generated/schema'
+import { ServiceDescription, ProposalDescription, ReviewDescription } from '../../generated/schema'
 
 //Adds metadata from ipfs as a entity called Description.
 //The description entity has the id of the cid to the file on IPFS
@@ -136,5 +136,58 @@ export function handleProposalData(content: Bytes): void {
     }
   }
   description.save()
+}
 
+export function handleReviewData(content: Bytes): void {
+  let context = dataSource.context();
+  let reviewId = context.getString('reviewId');
+  let cid = dataSource.stringParam()
+  let description = new ReviewDescription(cid)
+
+  if(reviewId){
+    var reviews = description.reviews
+    if(reviews){
+      reviews.push(reviewId)
+    } else {
+      reviews = [reviewId]
+    }
+    description.reviews = reviews
+  } else {
+    log.error("Requsted a reviewId, but none was given.", [])
+    return
+  }
+
+  const jsonObject = json.fromBytes(content).toObject();
+
+  if(jsonObject){
+    
+    //------- TO BE REMOVED: USED DURING DEV -------
+    /*Adds the information about which keys are present in the entry
+    This is done as a part of the PoC to show the current diversity of entries.
+    We currently have the following keys
+    expectedHours, proposalAbout, proposalTitle, rateType, description
+    ..for that reason we can include all of them in the entity.
+    We need to make a decision on that.*/
+    let s = "["
+    for(let i = 0; i < jsonObject.entries.length; i++){
+      if(i>0){ s += ", " }
+      let key = jsonObject.entries[i].key
+      s += key.toString();
+    }
+    s += "]"
+    description.keys = s
+  }
+
+  //description.content
+  let contentData = jsonObject.get('content')
+  if(contentData){
+    description.content = contentData.toString();
+  }
+  //description.rating
+  let rating = jsonObject.get('rating')
+  if(rating){
+    description.rating = rating.toBigInt();
+  }
+
+  description.save()
 }
