147db4a0532865b31ba819b0910e37f4ac34df7a
Quentin D.C <quent043@hotmail.com>
2023-01-27 10:00:29 +0000

    Refactor

--- src/mappings/EPNSNotification.ts
+++ src/mappings/EPNSNotification.ts
@@ -1,11 +1,10 @@
-import { BigInt, log } from "@graphprotocol/graph-ts"
+import { BigInt, log } from '@graphprotocol/graph-ts'
 import { EpnsNotificationCounter, EpnsPushNotification } from '../../generated/schema'
 
 // const SUB_GRAPH_ID = 'quent043/test'
 const SUB_GRAPH_ID = 'talentlayer/talent-layer-protocol'
 
-export function sendEPNSNotification(recipient: string, notification: string): void
-{
+export function sendEPNSNotification(recipient: string, notification: string): void {
   log.info('New id of EpnsNotificationCounter is: {}', [SUB_GRAPH_ID])
 
   let epnsNotificationCounter = EpnsNotificationCounter.load(SUB_GRAPH_ID)
@@ -13,15 +12,15 @@ export function sendEPNSNotification(recipient: string, notification: string): v
     epnsNotificationCounter = new EpnsNotificationCounter(SUB_GRAPH_ID)
     epnsNotificationCounter.totalCount = BigInt.fromI32(0)
   }
-  epnsNotificationCounter.totalCount = (epnsNotificationCounter.totalCount).plus(BigInt.fromI32(1))
+  epnsNotificationCounter.totalCount = epnsNotificationCounter.totalCount.plus(BigInt.fromI32(1))
 
   let count = epnsNotificationCounter.totalCount.toHexString()
-  let id2 = `${SUB_GRAPH_ID}+${count}`
-  log.info('New id of EpnsPushNotification is: {}', [id2])
+  let notificationId = `${SUB_GRAPH_ID}+${count}`
+  log.info('New id of EpnsPushNotification is: {}', [notificationId])
 
-  let epnsPushNotification = EpnsPushNotification.load(id2)
+  let epnsPushNotification = EpnsPushNotification.load(notificationId)
   if (epnsPushNotification == null) {
-    epnsPushNotification = new EpnsPushNotification(id2)
+    epnsPushNotification = new EpnsPushNotification(notificationId)
   }
 
   epnsPushNotification.recipient = recipient
