2718b0dd8f05c8cf4f8e6315b444f848eb3e5269
Emil Madrell <empasa@tuta.io>
2023-02-13 12:04:41 +0000

    unit tests for services and proposals

--- tests/ipfs-data/mock-events.ts
+++ tests/ipfs-data/mock-events.ts
@@ -1,6 +1,6 @@
-import { ethereum, BigInt } from "@graphprotocol/graph-ts"
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
 import { newMockEvent } from "matchstick-as"
-import { ServiceDataCreated } from "../../generated/ServiceRegistry/ServiceRegistry"
+import { ProposalCreated, ServiceCreated, ServiceDataCreated } from "../../generated/ServiceRegistry/ServiceRegistry"
 
 export function createNewServiceDataCreatedEvent(id: BigInt, serviceDataUri: string): ServiceDataCreated {
   let mockEvent = newMockEvent()
@@ -24,3 +24,75 @@ export function createNewServiceDataCreatedEvent(id: BigInt, serviceDataUri: str
 
   return serviceDataCreatedEvent
 } 
+
+export function createNewServiceCreatedEvent(id: BigInt, buyerId: BigInt, initiatorId: BigInt, platformId: BigInt, sellerId: BigInt): ServiceCreated {
+  let mockEvent = newMockEvent()
+  let serviceCreatedEvent = new ServiceCreated(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters,
+    null
+  )
+  
+  serviceCreatedEvent.parameters = new Array()
+  let idParam = new ethereum.EventParam('id', ethereum.Value.fromUnsignedBigInt(id))
+  let buyerIdParam = new ethereum.EventParam('buyerId', ethereum.Value.fromUnsignedBigInt(buyerId))
+  let initiatorIdParam = new ethereum.EventParam('initiatorId', ethereum.Value.fromUnsignedBigInt(initiatorId))
+  let platformIdParam = new ethereum.EventParam('platformId', ethereum.Value.fromUnsignedBigInt(platformId))
+  let sellerIdParam = new ethereum.EventParam('sellerId', ethereum.Value.fromUnsignedBigInt(sellerId))
+
+  serviceCreatedEvent.parameters.push(idParam)
+  serviceCreatedEvent.parameters.push(buyerIdParam)
+  serviceCreatedEvent.parameters.push(initiatorIdParam)
+  serviceCreatedEvent.parameters.push(platformIdParam)
+  serviceCreatedEvent.parameters.push(sellerIdParam)
+
+  return serviceCreatedEvent
+}
+
+export function createNewProposalCreatedEvent(serviceId: BigInt, sellerId: BigInt, rateToken: Address, rateAmount: BigInt, cid: string): ProposalCreated {
+  let mockEvent = newMockEvent()
+  let proposalCreatedEvent = new ProposalCreated(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters,
+    null
+  )
+
+  proposalCreatedEvent.parameters = new Array()
+  let serviceIdParam = getUintParam('serviceId', serviceId)
+  let sellerIdParam = getUintParam('sellerId', sellerId)
+  let rateTokenParam = getAddressParam('rateToken', rateToken)
+  let rateAmountParam = getUintParam('rateAmount', rateAmount)
+  let cidParam = getStringParam('proposalDataUri', cid)
+
+  proposalCreatedEvent.parameters.push(serviceIdParam)
+  proposalCreatedEvent.parameters.push(sellerIdParam)
+  proposalCreatedEvent.parameters.push(rateTokenParam)
+  proposalCreatedEvent.parameters.push(rateAmountParam)
+  proposalCreatedEvent.parameters.push(cidParam)
+
+  return proposalCreatedEvent
+}
+
+// --------------- help functions ------------------
+
+function getUintParam(param: string, i: BigInt): ethereum.EventParam {
+  return new ethereum.EventParam(param, ethereum.Value.fromUnsignedBigInt(i))
+}
+
+function getStringParam(param: string, s: string): ethereum.EventParam {
+  return new ethereum.EventParam(param, ethereum.Value.fromString(s))
+}
+
+function getAddressParam(param: string, a: Address): ethereum.EventParam {
+  return new ethereum.EventParam(param, ethereum.Value.fromAddress(a))
+}
--- tests/ipfs-data/service-description.test.ts
+++ tests/ipfs-data/service-description.test.ts
@@ -1,34 +1,62 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { afterAll, assert, beforeAll, clearStore, describe, test } from "matchstick-as/assembly/index"
+import { BigInt, ethereum, ipfs, json } from "@graphprotocol/graph-ts"
+import { afterAll, assert, beforeAll, clearStore, describe, mockIpfsFile, test } from "matchstick-as/assembly/index"
+import { getOrCreateService } from "../../src/getters"
+import { createServiceDescription } from "../../src/mappings/ipfs-data"
 import { handleServiceDataCreated } from "../../src/mappings/service-registry"
 import { createNewServiceDataCreatedEvent } from "./mock-events"
-
 
 beforeAll(() => {
     //mock contract calls
 })
 
 const serviceId = new BigInt(1)
-const cid = 'cid'
+const cid = "QmbViMcN9WiQhiFRuzMY3vVEJaG2yjD887zKFPVgYiogbw" // Stored in ./ipfs-mock-files/
 
 describe("handleServiceCreated()", () => {
   beforeAll(() => {
-    let newCreateNewServiceDataCreatedEvent = createNewServiceDataCreatedEvent(
+    const path = 'tests/ipfs-data/ipfs-mock-files/service-data'
+    
+    mockIpfsFile(cid, path + '/' + cid + '.json')
+    
+    assert.entityCount('ServiceDescription', 0)
+    assert.entityCount('Service', 0)
+
+    let serviceDataCreatedEvent = createNewServiceDataCreatedEvent(
       serviceId,
       cid
     )
-    handleServiceDataCreated(newCreateNewServiceDataCreatedEvent)
+
+    handleServiceDataCreated(serviceDataCreatedEvent)
+
+    assert.entityCount('Service', 1)
+    // assert.entityCount('ServiceDescription', 1)
   })
   
   afterAll(() => {
     clearStore()
   })
-
-  test("Should create a new Service entity", () => {
-    assert.fieldEquals('Service', serviceId.toString(), 'cid', cid)
+  
+  test('ipfs.cat', () => {
+    assert.entityCount('ServiceDescription', 0)
+    
+    let serviceData = ipfs.cat(cid)
+    
+    if(!serviceData){ return }
+    
+    const jsonObject = json.fromBytes(serviceData).toObject()
+    
+    getOrCreateService(serviceId)
+  
+    createServiceDescription(jsonObject, cid, serviceId)
+  
+    assert.entityCount('ServiceDescription', 1)
   })
 
-  test("Shuold create a new ServiceDescription entity", () => {
-    assert.fieldEquals('ServiceDescription', cid, 'service', serviceId.toString())
+  test('ServiceDescription Success Fields', () => {
+    assert.fieldEquals('ServiceDescription', cid, 'title', 'title')
+    assert.fieldEquals('ServiceDescription', cid, 'about', 'about')
+    assert.fieldEquals('ServiceDescription', cid, 'keywords_raw', 'keyword a, keyword b')
+    assert.fieldEquals('ServiceDescription', cid, 'rateToken', '0x0000000000000000000000000000000000000000')
+    assert.fieldEquals('ServiceDescription', cid, 'rateAmount', "1")
   })
 })
