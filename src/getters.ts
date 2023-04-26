1dbaa36b9f8d03e0a817c3bf5e633f87d6a96d57
Quentin D.C <quent043@hotmail.com>
2023-04-26 17:09:58 +0100

    Added missing ID
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/getters.ts
+++ src/getters.ts
@@ -73,6 +73,7 @@ export function getOrCreateUser(id: BigInt): User {
     user.createdAt = ZERO
     user.updatedAt = ZERO
     user.delegates = []
+    user.userStats = userStats.id
     user.save()
   }
 
