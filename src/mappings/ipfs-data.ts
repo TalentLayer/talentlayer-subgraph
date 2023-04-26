81869662d5996775a9423b4ccbc99b35c0a2d130
0xRomain <romain@talentlayer.org>
2023-04-26 16:06:03 +0200

    fix wrong commet line

--- src/mappings/ipfs-data.ts
+++ src/mappings/ipfs-data.ts
@@ -128,7 +128,7 @@ export function handleUserData(content: Bytes): void {
 
   //Creates duplicate values. Open issue
   //https://github.com/graphprotocol/graph-node/issues/4087
-  // //description.skills = createKeywordEntities(description.skills_raw!)!
+  //description.skills = createKeywordEntities(description.skills_raw!)!
 
   description.save()
 }
@@ -182,7 +182,7 @@ function getValueAsBigInt(jsonObject: TypedMap<string, JSONValue>, key: string):
 }
 
 //Transforms a comma separated string of keywords into an Array of Keyword.id entities.
-// function createKeywordEntities(keywords: string): string[] | null {
+function createKeywordEntities(keywords: string): string[] | null {
   const _keywords = keywords.split(',')
 
   //To avoid returning an empty list, which is not allowed according to the schema.
