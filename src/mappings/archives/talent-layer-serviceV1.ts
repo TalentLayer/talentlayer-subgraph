d54df8867cd2cc9b7c69b5bfdcc248a900a68ae7
Quentin D.C <quent043@hotmail.com>
2023-06-15 22:20:34 +0200

    Deleted V1 mapper
    Signed-off-by: Quentin D.C <quent043@hotmail.com>

--- src/mappings/archives/talent-layer-serviceV1.ts
+++ src/mappings/archives/talent-layer-serviceV1.ts
@@ -1,132 +1,132 @@
-import { store, DataSourceContext } from '@graphprotocol/graph-ts'
-import { Platform } from '../../../generated/schema'
-import { ServiceData, ProposalData } from '../../../generated/templates'
-import {
-  getOrCreateService,
-  getOrCreateProposal,
-  getOrCreateToken,
-  getOrCreatePlatform,
-  getOrCreateUser,
-  getOrCreateUserStat,
-} from '../../getters'
-import { generateIdFromTwoElements } from '../utils'
-import { ONE } from '../../constants'
-import {
-  ProposalUpdated as ProposalUpdatedV1,
-  ProposalCreated as ProposalCreatedV1,
-  ServiceCreated as ServiceCreatedV1,
-  ServiceDetailedUpdated,
-} from '../../../generated/templates/ServiceData/TalentLayerServiceV1'
-
-export function handleServiceCreatedV1(event: ServiceCreatedV1): void {
-  const buyerStats = getOrCreateUserStat(event.params.ownerId)
-  buyerStats.numCreatedServices = buyerStats.numCreatedServices.plus(ONE)
-  buyerStats.save()
-
-  const service = getOrCreateService(event.params.id)
-  service.createdAt = event.block.timestamp
-  service.updatedAt = event.block.timestamp
-  service.buyer = getOrCreateUser(event.params.ownerId).id
-  service.status = 'Opened'
-  const platform = getOrCreatePlatform(event.params.platformId)
-  service.platform = platform.id
-  service.cid = event.params.dataUri
-  const dataId = event.params.dataUri + '-' + event.block.timestamp.toString()
-
-  const context = new DataSourceContext()
-  context.setBigInt('serviceId', event.params.id)
-  context.setString('id', dataId)
-  ServiceData.createWithContext(event.params.dataUri, context)
-
-  service.description = dataId
-  service.save()
-}
-
-export function handleServiceDetailedUpdated(event: ServiceDetailedUpdated): void {
-  const serviceId = event.params.id
-  const service = getOrCreateService(serviceId)
-  const oldCid = service.cid
-  const newCid = event.params.dataUri
-  const dataId = newCid + '-' + event.block.timestamp.toString()
-
-  //service.created set in handleServiceCreated.
-  service.updatedAt = event.block.timestamp
-  service.cid = newCid
-
-  const context = new DataSourceContext()
-  context.setBigInt('serviceId', serviceId)
-  context.setString('id', dataId)
-
-  if (oldCid) {
-    store.remove('ServiceDescription', oldCid)
-  }
-
-  ServiceData.createWithContext(newCid, context)
-
-  service.description = dataId
-  service.save()
-}
-
-export function handleProposalCreatedV1(event: ProposalCreatedV1): void {
-  const sellerStats = getOrCreateUserStat(event.params.ownerId)
-  sellerStats.numCreatedProposals = sellerStats.numCreatedProposals.plus(ONE)
-  sellerStats.save()
-
-  const proposalId = generateIdFromTwoElements(event.params.serviceId.toString(), event.params.ownerId.toString())
-  const proposal = getOrCreateProposal(proposalId, event.params.serviceId)
-  proposal.status = 'Pending'
-
-  proposal.service = getOrCreateService(event.params.serviceId).id
-  proposal.seller = getOrCreateUser(event.params.ownerId).id
-  // Handled in getOrCreateProposal
-  proposal.rateToken = getOrCreateToken(event.params.rateToken).id
-  proposal.rateAmount = event.params.rateAmount
-  proposal.platform = Platform.load(event.params.platformId.toString())!.id
-  proposal.expirationDate = event.params.expirationDate
-
-  proposal.createdAt = event.block.timestamp
-  proposal.updatedAt = event.block.timestamp
-
-  const cid = event.params.dataUri
-  proposal.cid = cid
-
-  const dataId = cid + '-' + event.block.timestamp.toString()
-  proposal.description = dataId
-
-  const context = new DataSourceContext()
-  context.setString('proposalId', proposalId)
-  context.setString('id', dataId)
-  ProposalData.createWithContext(cid, context)
-
-  proposal.save()
-}
-
-export function handleProposalUpdatedV1(event: ProposalUpdatedV1): void {
-  const token = event.params.rateToken
-  const proposalId = generateIdFromTwoElements(event.params.serviceId.toString(), event.params.ownerId.toString())
-  const proposal = getOrCreateProposal(proposalId, event.params.serviceId)
-  const newCid = event.params.dataUri
-  const oldCid = proposal.cid
-  const dataId = newCid + '-' + event.block.timestamp.toString()
-
-  proposal.rateToken = getOrCreateToken(token).id
-  proposal.rateAmount = event.params.rateAmount
-
-  //proposal.created set in handleProposalCreated.
-  proposal.updatedAt = event.block.timestamp
-
-  proposal.cid = newCid
-  proposal.expirationDate = event.params.expirationDate
-  const context = new DataSourceContext()
-  context.setString('proposalId', proposalId)
-  context.setString('id', dataId)
-
-  if (oldCid) {
-    store.remove('ProposalDescription', oldCid)
-  }
-
-  ProposalData.createWithContext(newCid, context)
-
-  proposal.description = dataId
-  proposal.save()
-}
+// import { store, DataSourceContext } from '@graphprotocol/graph-ts'
+// import { Platform } from '../../../generated/schema'
+// import { ServiceData, ProposalData } from '../../../generated/templates'
+// import {
+//   getOrCreateService,
+//   getOrCreateProposal,
+//   getOrCreateToken,
+//   getOrCreatePlatform,
+//   getOrCreateUser,
+//   getOrCreateUserStat,
+// } from '../../getters'
+// import { generateIdFromTwoElements } from '../utils'
+// import { ONE } from '../../constants'
+// import {
+//   ProposalUpdated as ProposalUpdatedV1,
+//   ProposalCreated as ProposalCreatedV1,
+//   ServiceCreated as ServiceCreatedV1,
+//   ServiceDetailedUpdated,
+// } from '../../../generated/templates/ServiceData/TalentLayerServiceV1'
+//
+// export function handleServiceCreatedV1(event: ServiceCreatedV1): void {
+//   const buyerStats = getOrCreateUserStat(event.params.ownerId)
+//   buyerStats.numCreatedServices = buyerStats.numCreatedServices.plus(ONE)
+//   buyerStats.save()
+//
+//   const service = getOrCreateService(event.params.id)
+//   service.createdAt = event.block.timestamp
+//   service.updatedAt = event.block.timestamp
+//   service.buyer = getOrCreateUser(event.params.ownerId).id
+//   service.status = 'Opened'
+//   const platform = getOrCreatePlatform(event.params.platformId)
+//   service.platform = platform.id
+//   service.cid = event.params.dataUri
+//   const dataId = event.params.dataUri + '-' + event.block.timestamp.toString()
+//
+//   const context = new DataSourceContext()
+//   context.setBigInt('serviceId', event.params.id)
+//   context.setString('id', dataId)
+//   ServiceData.createWithContext(event.params.dataUri, context)
+//
+//   service.description = dataId
+//   service.save()
+// }
+//
+// export function handleServiceDetailedUpdated(event: ServiceDetailedUpdated): void {
+//   const serviceId = event.params.id
+//   const service = getOrCreateService(serviceId)
+//   const oldCid = service.cid
+//   const newCid = event.params.dataUri
+//   const dataId = newCid + '-' + event.block.timestamp.toString()
+//
+//   //service.created set in handleServiceCreated.
+//   service.updatedAt = event.block.timestamp
+//   service.cid = newCid
+//
+//   const context = new DataSourceContext()
+//   context.setBigInt('serviceId', serviceId)
+//   context.setString('id', dataId)
+//
+//   if (oldCid) {
+//     store.remove('ServiceDescription', oldCid)
+//   }
+//
+//   ServiceData.createWithContext(newCid, context)
+//
+//   service.description = dataId
+//   service.save()
+// }
+//
+// export function handleProposalCreatedV1(event: ProposalCreatedV1): void {
+//   const sellerStats = getOrCreateUserStat(event.params.ownerId)
+//   sellerStats.numCreatedProposals = sellerStats.numCreatedProposals.plus(ONE)
+//   sellerStats.save()
+//
+//   const proposalId = generateIdFromTwoElements(event.params.serviceId.toString(), event.params.ownerId.toString())
+//   const proposal = getOrCreateProposal(proposalId, event.params.serviceId)
+//   proposal.status = 'Pending'
+//
+//   proposal.service = getOrCreateService(event.params.serviceId).id
+//   proposal.seller = getOrCreateUser(event.params.ownerId).id
+//   // Handled in getOrCreateProposal
+//   proposal.rateToken = getOrCreateToken(event.params.rateToken).id
+//   proposal.rateAmount = event.params.rateAmount
+//   proposal.platform = Platform.load(event.params.platformId.toString())!.id
+//   proposal.expirationDate = event.params.expirationDate
+//
+//   proposal.createdAt = event.block.timestamp
+//   proposal.updatedAt = event.block.timestamp
+//
+//   const cid = event.params.dataUri
+//   proposal.cid = cid
+//
+//   const dataId = cid + '-' + event.block.timestamp.toString()
+//   proposal.description = dataId
+//
+//   const context = new DataSourceContext()
+//   context.setString('proposalId', proposalId)
+//   context.setString('id', dataId)
+//   ProposalData.createWithContext(cid, context)
+//
+//   proposal.save()
+// }
+//
+// export function handleProposalUpdatedV1(event: ProposalUpdatedV1): void {
+//   const token = event.params.rateToken
+//   const proposalId = generateIdFromTwoElements(event.params.serviceId.toString(), event.params.ownerId.toString())
+//   const proposal = getOrCreateProposal(proposalId, event.params.serviceId)
+//   const newCid = event.params.dataUri
+//   const oldCid = proposal.cid
+//   const dataId = newCid + '-' + event.block.timestamp.toString()
+//
+//   proposal.rateToken = getOrCreateToken(token).id
+//   proposal.rateAmount = event.params.rateAmount
+//
+//   //proposal.created set in handleProposalCreated.
+//   proposal.updatedAt = event.block.timestamp
+//
+//   proposal.cid = newCid
+//   proposal.expirationDate = event.params.expirationDate
+//   const context = new DataSourceContext()
+//   context.setString('proposalId', proposalId)
+//   context.setString('id', dataId)
+//
+//   if (oldCid) {
+//     store.remove('ProposalDescription', oldCid)
+//   }
+//
+//   ProposalData.createWithContext(newCid, context)
+//
+//   proposal.description = dataId
+//   proposal.save()
+// }
